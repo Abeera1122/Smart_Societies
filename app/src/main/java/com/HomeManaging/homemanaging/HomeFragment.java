1fb7960c783a1619d4d4a546730c753bb1b77e74
FahadHassn <119291468+FahadHassn@users.noreply.github.com>
2022-12-15 19:41:39 +0500

    horizontal recyclerview

--- app/src/main/java/com/HomeManaging/homemanaging/HomeFragment.java
+++ app/src/main/java/com/HomeManaging/homemanaging/HomeFragment.java
@@ -19,11 +19,16 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     RecyclerView RentHouseRecyclerView;
-    private List<RentHouseModel> rentHouseModelList;
+    private List<RentHouseModel> rentHouseModelList1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        rentHouseModelList1 = new ArrayList<>();
+        rentHouseModelList1.add(new RentHouseModel(R.drawable.wall1,"Farm House","2","5","Sialkot"));
+        rentHouseModelList1.add(new RentHouseModel(R.drawable.wall1,"Farm House","2","5","Sialkot"));
+        rentHouseModelList1.add(new RentHouseModel(R.drawable.wall1,"Farm House","2","5","Sialkot"));
     }
 
     @Override
@@ -31,14 +36,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        rentHouseModelList = new ArrayList<>();
-        rentHouseModelList.add(new RentHouseModel(R.id._rent_house_image,"Farm House","2","5","Sialkot"));
-        rentHouseModelList.add(new RentHouseModel(R.id._rent_house_image,"Farm House","2","5","Sialkot"));
-        rentHouseModelList.add(new RentHouseModel(R.id._rent_house_image,"Farm House","2","5","Sialkot"));
-
         RentHouseRecyclerView = v.findViewById(R.id.rent_recycler);
         RentHouseRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-        RentHouseAdapter rentHouseAdapter = new RentHouseAdapter(getContext(),rentHouseModelList);
+        RentHouseAdapter rentHouseAdapter = new RentHouseAdapter(getContext(),rentHouseModelList1);
         RentHouseRecyclerView.setAdapter(rentHouseAdapter);
         return v;
     }
--- app/src/main/java/com/HomeManaging/homemanaging/Model/RentHouseModel.java
+++ app/src/main/java/com/HomeManaging/homemanaging/Model/RentHouseModel.java
@@ -2,54 +2,54 @@ package com.HomeManaging.homemanaging.Model;
 
 public class RentHouseModel {
 
-    int House_image;
-    String House_name, House_BRooms, House_MRooms,House_City;
+    Integer house_image;
+    String house_name, house_BRooms, house_MRooms,house_City;
 
     public RentHouseModel(int house_image, String house_name, String house_BRooms, String house_MRooms, String house_City) {
-        House_image = house_image;
-        House_name = house_name;
-        House_BRooms = house_BRooms;
-        House_MRooms = house_MRooms;
-        House_City = house_City;
+        this.house_image = house_image;
+        this.house_name = house_name;
+        this.house_BRooms = house_BRooms;
+        this.house_MRooms = house_MRooms;
+        this.house_City = house_City;
     }
 
     public int getHouse_image() {
-        return House_image;
+        return house_image;
     }
 
     public void setHouse_image(int house_image) {
-        House_image = house_image;
+        this.house_image = house_image;
     }
 
     public String getHouse_name() {
-        return House_name;
+        return house_name;
     }
 
     public void setHouse_name(String house_name) {
-        House_name = house_name;
+        this.house_name = house_name;
     }
 
     public String getHouse_BRooms() {
-        return House_BRooms;
+        return house_BRooms;
     }
 
     public void setHouse_BRooms(String house_BRooms) {
-        House_BRooms = house_BRooms;
+        this.house_BRooms = house_BRooms;
     }
 
     public String getHouse_MRooms() {
-        return House_MRooms;
+        return house_MRooms;
     }
 
     public void setHouse_MRooms(String house_MRooms) {
-        House_MRooms = house_MRooms;
+        this.house_MRooms = house_MRooms;
     }
 
     public String getHouse_City() {
-        return House_City;
+        return house_City;
     }
 
     public void setHouse_City(String house_City) {
-        House_City = house_City;
+        this.house_City = house_City;
     }
 }
